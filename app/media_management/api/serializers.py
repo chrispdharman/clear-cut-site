bcfef37f1929a93f3c2bf893f023d3f210fea56d
chrispdharman <christopher.p.d.harman@gmail.com>
2020-04-11 13:03:34 +0100

    Serializing media items output

--- app/media_management/api/serializers.py
+++ app/media_management/api/serializers.py
@@ -1,10 +1,27 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
+from media_management import constants
 from media_management.models import MediaItem
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'email', )
+
+
 class MediaItemSerializer(serializers.ModelSerializer):
+    media_type = serializers.SerializerMethodField()
+    user = UserSerializer()
+
     class Meta:
         model = MediaItem
         fields = '__all__'
+        depth = 1
+
+    def get_media_type(self, obj):
+        for element in constants.ALLOWED_MEDIA_TYPES:
+            if element[0] == obj.media_type:
+                return element[1]
     
