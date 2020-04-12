e7d2d8531834391a7c44070fc9b9b3747bc65a48
chrispdharman <christopher.p.d.harman@gmail.com>
2020-04-12 12:51:10 +0100

    Fix serializer so that creating media item allows media type selector

--- app/media_management/api/serializers.py
+++ app/media_management/api/serializers.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from clear_cut.api.serializers import ClearCutConfigSerializer
+from clear_cut.models import ClearCutConfig
 from media_management import constants
 from media_management.models import MediaItem
 
@@ -13,7 +14,7 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class MediaItemSerializer(serializers.ModelSerializer):
-    media_type = serializers.SerializerMethodField()
+    media_type_name = serializers.SerializerMethodField()
 
     clear_cut_config = ClearCutConfigSerializer(read_only=True)
     user = UserSerializer(read_only=True)
@@ -22,8 +23,13 @@ class MediaItemSerializer(serializers.ModelSerializer):
         model = MediaItem
         fields = '__all__'
         depth = 1
+    
+    def create(self, validated_data):
+        validated_data['clear_cut_config'] = ClearCutConfig.objects.get(is_default=True)
+        validated_data['user'] = self.context['request'].user
+        return super(MediaItemSerializer, self).create(validated_data)
 
-    def get_media_type(self, obj):
+    def get_media_type_name(self, obj):
         for element in constants.ALLOWED_MEDIA_TYPES:
             if element[0] == obj.media_type:
                 return element[1]
--- app/media_management/api/views.py
+++ app/media_management/api/views.py
@@ -8,6 +8,16 @@ class ListCreateMediaItemView(generics.ListCreateAPIView):
     queryset = MediaItem.objects.all()
     serializer_class = MediaItemSerializer
 
+    def get_serializer_context(self):
+        """
+        Extra context provided to the serializer class.
+        """
+        return {
+            'request': self.request,
+            'format': self.format_kwarg,
+            'view': self
+        }  
+
 class RetrieveUpdateDestroyMediaItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = MediaItem.objects.all()
     serializer_class = MediaItemSerializer
