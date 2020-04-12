d60ee106393216e3ab4a71e978b9752248846d51
chrispdharman <christopher.p.d.harman@gmail.com>
2020-04-12 14:10:31 +0100

    Including js in attempt to convert media type into intelligible strings

---
+++ app/media_management/static/dashboard/utils.js
@@ -0,0 +1,25 @@
+function convertMediaType(params) {
+    console.log("params", params);
+    var media_type_value = params[0];
+    if (Number.isInteger(media_type_value)) {
+        switch (media_type_value) {
+            case "1":
+                return "IMAGE";
+            case "2":
+                return "VIDEO";
+            default:
+                return "UNKNOWN";
+        }
+    }
+}
+
+function init() {
+    var media_type_elements = document.getElementsByClassName("media-type-to-name");
+    var media_type_index = media_type_elements.length;
+
+    while(media_type_index--) {
+        var element = media_type_elements[media_type_index];
+        var mediaType = element.getAttribute("data-media-type");
+        element.innerHTML = convertMediaType(mediaType);
+    };
+}
