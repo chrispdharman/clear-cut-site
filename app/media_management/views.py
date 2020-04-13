d7341d0d80f5ef4b04b7e7c81fcc1f7619fe6b38
chrispdharman <christopher.p.d.harman@gmail.com>
2020-04-13 23:22:53 +0100

    Attempt to use DRF serializer for html form

--- app/media_management/views.py
+++ app/media_management/views.py
@@ -2,12 +2,17 @@ from django.http import HttpResponse
 from django.template import loader
 
 from media_management.models import MediaItem
+from media_management.api.serializers import MediaItemSerializer
 
 
 def index(request):
-    recent_media_items = MediaItem.objects.order_by('-id')
     template = loader.get_template('dashboard/index.html')
+
+    recent_media_items = MediaItem.objects.order_by('-id')
+    serializer = MediaItemSerializer(recent_media_items[0])
+
     context = {
         'media_items': recent_media_items,
+        'serializer': serializer,
     }
     return HttpResponse(template.render(context, request))
