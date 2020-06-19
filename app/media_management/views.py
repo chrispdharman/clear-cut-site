f49948384712f6cf12c150f32dc1c5d09d59a53f
chrispdharman <christopher.p.d.harman@gmail.com>
2020-06-19 22:39:57 +0100

    Tidy up and don't use serializer for parsing new content (for now)

--- app/media_management/views.py
+++ app/media_management/views.py
@@ -2,17 +2,14 @@ from django.http import HttpResponse
 from django.template import loader
 
 from media_management.models import MediaItem
-from media_management.api.serializers import MediaItemSerializer
 
 
 def index(request):
     template = loader.get_template('dashboard/index.html')
 
     recent_media_items = MediaItem.objects.order_by('-id')
-    serializer = MediaItemSerializer(recent_media_items[0])
 
     context = {
         'media_items': recent_media_items,
-        'serializer': serializer,
     }
     return HttpResponse(template.render(context, request))
