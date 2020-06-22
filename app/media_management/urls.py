eeb29e22f1f9ed06927fadd5844582bb634c0f18
chrispdharman <christopher.p.d.harman@gmail.com>
2020-06-22 18:55:35 +0100

    Linking Upload through to new page + minor tidy up

--- app/media_management/urls.py
+++ app/media_management/urls.py
@@ -5,6 +5,6 @@ from media_management import views
 
 urlpatterns = [
     path('api/', include('media_management.api.urls')),
-    path('upload/', views.uploader, name='uploader'),
+    path('upload/', views.uploader, name='item_uploader'),
     path('', views.index, name='index'),
 ]
--- app/media_management/views.py
+++ app/media_management/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
 
+from media_management.api.serializers import MediaItemSerializer
 from media_management.models import MediaItem
 
 
@@ -18,8 +19,10 @@ def uploader(request):
     template = loader.get_template('manage/uploader.html')
 
     recent_media_items = MediaItem.objects.order_by('-id')
+    serializer = MediaItemSerializer(recent_media_items[0])
 
     context = {
         'media_items': recent_media_items,
+        'serializer': serializer,
     }
     return HttpResponse(template.render(context, request))
