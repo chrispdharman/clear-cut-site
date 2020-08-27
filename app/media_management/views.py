d9089c1ab3f7a2f22fb9c8cb791e1e8e39d1e8f7
chrispdharman <christopher.p.d.harman@gmail.com>
2020-08-27 20:18:08 +0100

    Adding navigation menu

--- app/media_management/views.py
+++ app/media_management/views.py
@@ -5,20 +5,32 @@ from media_management.api.serializers import MediaItemSerializer
 from media_management.models import MediaItem
 
 
+def _parse_context(additional_context=None):
+    context = {
+        'menu_logo': 'https://clear-cut.s3.eu-west-2.amazonaws.com/results/test/manual-clear-cut-logo/0008_edge_masked_image.png',
+    }
+
+    if additional_context:
+        context.update(additional_context)
+
+    return context
+
+
 def index(request):
     template = loader.get_template('manage/index.html')
 
     recent_media_items = MediaItem.objects.order_by('-id')
 
-    context = {
+    context = _parse_context({
         'media_items': recent_media_items,
-    }
+    })
     return HttpResponse(template.render(context, request))
+
 
 def uploader(request):
     template = loader.get_template('manage/uploader.html')
 
-    context = {
+    context = _parse_context({
         'serializer': MediaItemSerializer(),
-    }
+    })
     return HttpResponse(template.render(context, request))
